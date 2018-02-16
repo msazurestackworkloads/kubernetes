7574ad94d9c130d2a5abfaf814323ef6f62c12cd
seyadava <seyadava@microsoft.com>
2018-02-16 15:08:31 -0800

    util fix

--- pkg/cloudprovider/providers/azure/azure_util.go
+++ pkg/cloudprovider/providers/azure/azure_util.go
@@ -288,7 +288,7 @@ func getProtocolsFromKubernetesProtocol(protocol v1.Protocol) (*network.Transpor
 		return &transportProto, &securityProto, &probeProto, nil
 	case v1.ProtocolUDP:
 		transportProto = network.TransportProtocolUDP
-		securityProto = network.UDP
+		securityProto = network.SecurityRuleProtocolUDP
 		return &transportProto, &securityProto, nil, nil
 	default:
 		return &transportProto, &securityProto, &probeProto, fmt.Errorf("Only TCP and UDP are supported for Azure LoadBalancers")
