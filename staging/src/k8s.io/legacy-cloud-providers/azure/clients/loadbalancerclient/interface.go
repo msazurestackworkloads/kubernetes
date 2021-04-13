62e6976e4217d3a3aa40dd17c168dd640960bd01
JesusAlvarezTorres <jesusa@microsoft.com>
2021-04-13 11:25:22 -0700

    Azure Stack cloud provider API version changes

--- staging/src/k8s.io/legacy-cloud-providers/azure/clients/loadbalancerclient/interface.go
+++ staging/src/k8s.io/legacy-cloud-providers/azure/clients/loadbalancerclient/interface.go
@@ -27,7 +27,7 @@ import (
 
 const (
 	// APIVersion is the API version for network.
-	APIVersion = "2019-06-01"
+	APIVersion = "2018-11-01"
 )
 
 // Interface is the client interface for LoadBalancer.
